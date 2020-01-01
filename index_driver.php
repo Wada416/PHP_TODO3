95f87f7dd9251aac709b50d2086ae0776b70a729
Wada416 <wada1982s@gmail.com>
2020-01-01 17:56:44 +0900

    index_driver.php作成　index.phpで使うデータを用意してリダイレクトする

---
+++ index_driver.php
@@ -0,0 +1,8 @@
+<?php
+    require "DAO.php";
+    session_start();
+    $dao = new DAO;
+    $itemList = $dao->getItemList();
+    $_SESSION['itemList'] = $itemList;
+    header('Location: http://localhost/php_todo3/index.php'); 
+?>
