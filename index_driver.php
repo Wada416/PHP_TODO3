d38a5a6617645e7f8558f977c6598d90702c132d
Wada416 <wada1982s@gmail.com>
2020-01-02 23:44:48 +0900

    連想配列のコーディングテスト

--- index_driver.php
+++ index_driver.php
@@ -3,6 +3,11 @@
     session_start();
     $dao = new DAO;
     $itemList = $dao->getItemList();
-    $_SESSION['itemList'] = $itemList;
-    header('Location: http://localhost/php_todo3/index.php'); 
+    //$_SESSION['itemList'] = $itemList;
+    foreach ($itemList as $item){
+        $tableRow = array('name' => $item['name']);
+        echo $tableRow['name'];
+    }
+    
+    //header('Location: http://localhost/php_todo3/index.php'); 
 ?>
