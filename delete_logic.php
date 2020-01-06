235acc0a2a3ed7ecdbfa5755c8b190076edaa39b
Wada416 <wada1982s@gmail.com>
2020-01-06 23:32:04 +0900

    index_driverで行う処理を削除

--- delete_logic.php
+++ delete_logic.php
@@ -3,7 +3,5 @@
     session_start();
     $dao = new DAO;
     $dao->deleteItem($_POST['id']);
-    $itemList = $dao->getItemList();
-    $_SESSION['itemList'] = $itemList;
     header('Location: http://localhost/php_todo3/index_driver.php');
 ?>
